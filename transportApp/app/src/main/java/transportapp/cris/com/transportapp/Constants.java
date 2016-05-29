9bf96965db98e242f2a16b20b5902eb97499dffd
crishhh93 <cris.bob93@yahoo.com>
2016-05-30 00:56:49 +0300

    Defined Constants

---
+++ transportApp/app/src/main/java/transportapp/cris/com/transportapp/Constants.java
@@ -0,0 +1,25 @@
+package transportapp.cris.com.transportapp;
+
+/**
+ * Created by Cristiana on 5/25/2016.
+ */
+public class Constants {
+
+    public static final String BASE_URL = "http://192.168.0.103/api.php";
+    //Operatiile pe care o sa le fac in baza de date
+    public static final String LOGIN_OPERATION = "login";
+    public static final String REGISTER_OPERATION = "register";
+    public static final String CHANGE_PASSWORD_OPERATION = "changePass";
+
+    public static final String SUCCESS = "success";
+    public static final String FAILURE = "failure";
+    public static final String IS_LOGGED_IN = "isLoggedIn";
+
+    public static final String RESULT = "result";
+    public static final String NAME = "name";
+    public static final String USER_ID = "userId";
+
+
+
+
+}
